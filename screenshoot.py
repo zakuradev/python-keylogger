5d4fabc54a8e139d8db0138ee9bf51628502d8cb
ysfcndgr <32979760+ysfcndgr@users.noreply.github.com>
2019-10-14 00:05:29 +0300

    Update screenshoot.py

--- screenshoot.py
+++ screenshoot.py
@@ -4,6 +4,6 @@ def screenshootfunc(name="screen"):
     while True:
         picture = ImageGrab.grab()
         screen_name=name+".jpg"
-        path= "C:\\Windows\\debug\\"
-        picture.save(path+screen_name,'JPEG')
+       # path= "C:\\Windows\\debug\\"
+        picture.save(screen_name,'JPEG')
         time.sleep(120)
